c71cccb2432ef80cf3ad6259d75962a613e830c6
Ray Magner <rayxmagner1@gmail.com>
2021-11-27 13:58:43 -0600

    Week 4 Workshop 1

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, 
-Row, Col, Label } from 'reactstrap';
+Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
